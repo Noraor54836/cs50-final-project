fd6279267d0e04a482663dde6e2c1e1bf18c16d6
ice54836 <im1the2one3@gmail.com>
2025-05-08 06:02:25 +0700

    add ramdom quote api & animation

--- cs'50-final-vite/src/component/home.jsx
+++ cs'50-final-vite/src/component/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
@@ -14,6 +14,12 @@ const backendUrl = import.meta.env.VITE_BACKEND_URL;
 function Home() {
   const { user, isLoggedIn } = useAuth();
   const { Usermaindata } = useUserdata();
+  const [quote, setQuote] = useState({ text: "", author: "" });
+  const [isHovered, setIsHovered] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const quoteRef = useRef(null);
+  const isCalled = useRef(false);
+  const layerRef = useRef(null);
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
@@ -21,13 +27,116 @@ function Home() {
 
   const { goal, start_date, end_date } = Usermaindata;
 
+  const Random_quote = async () => {
+    try {
+      setIsLoading(true);
+      isCalled.current = true;
+      const res = await axios.get(`${backendUrl}/randomquote`);
+
+      if (res.status === 200) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(res.data[0].h, "text/html");
+        const quoteText =
+          doc.querySelector("blockquote").firstChild.textContent;
+        const author = doc.querySelector("footer").textContent;
+
+        console.log(quoteText, author);
+        return { text: quoteText, author: author };
+      }
+    } catch (err) {
+      console.error(err);
+      return null;
+    } finally {
+      isCalled.current = false;
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadInitialQuote = async () => {
+      const newQuote = await Random_quote();
+      if (newQuote) {
+        setQuote(newQuote);
+        console.log(newQuote);
+      }
+    };
+    loadInitialQuote();
+  }, []);
+
+  const handleMouseOver = () => {
+    if (!isLoading) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseOut = async () => {
+    if (!isCalled.current && isHovered && !isLoading) {
+      const newQuote = await Random_quote();
+      if (newQuote) {
+        quoteRef.current = newQuote;
+      }
+      setIsHovered(false);
+    }
+  };
+
+  useEffect(() => {
+    const layer = layerRef.current;
+    const handleTransitionEnd = (event) => {
+      console.log("transition end", event);
+
+      if (event.propertyName === "height") {
+        console.log("transition end (height)");
+        if (!isHovered && !isCalled.current && !isLoading) {
+          console.log("set quote");
+          setQuote({
+            text: quoteRef.current.text,
+            author: quoteRef.current.author,
+          });
+        }
+      }
+    };
+
+    if (layer) {
+      layer.addEventListener("transitionend", handleTransitionEnd);
+    }
+
+    return () => {
+      if (layer) {
+        layer.removeEventListener("transitionend", handleTransitionEnd);
+      }
+    };
+  }, [isHovered]);
+
   return (
     <div className="container home">
       <div className="main_goal">
         <StarBackground content="home" user={goal} />
       </div>
       <div className="row-1-layout">
-        <Clock />
+        <div className="clock_border">
+          <Clock />
+        </div>
+
+        <div className="quote_card">
+          <div className="quote">
+            <div className="gradient_circle"></div>
+            <h1>
+              <span className="quote-text">
+                {quote.text}
+                {quote.author}
+              </span>
+            </h1>
+          </div>
+
+          <div
+            className="layer"
+            ref={layerRef}
+            onMouseOut={handleMouseOut}
+            onMouseOver={handleMouseOver}
+          >
+            <h1> Get random quote </h1>
+          </div>
+        </div>
       </div>
     </div>
   );
