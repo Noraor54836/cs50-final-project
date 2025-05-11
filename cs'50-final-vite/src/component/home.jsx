7407db931262bcabd4a4af204ea64a954b4a3217
ice54836 <im1the2one3@gmail.com>
2025-05-12 05:01:45 +0700

    add checkin button

--- cs'50-final-vite/src/component/home.jsx
+++ cs'50-final-vite/src/component/home.jsx
@@ -18,6 +18,8 @@ function Home() {
   const [quote, setQuote] = useState({ text: "", author: "" });
   const [isHovered, setIsHovered] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [timeSpent, setTimeSpent] = useState("00:00:00");
 
   const quoteRef = useRef(null);
   const isCalled = useRef(false);
@@ -97,6 +99,12 @@ function Home() {
     }
   };
 
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+    // Reset time when toggled
+    setTimeSpent(isChecked ? "00:00:00" : "00:00:01");
+  };
+
   useEffect(() => {
     const layer = layerRef.current;
     const handleTransitionEnd = (event) => {
@@ -167,6 +175,47 @@ function Home() {
           <h1>
             {date.getDate()} {months[date.getMonth()]} {date.getFullYear()}
           </h1>
+        </div>
+
+        <div className="check-in-layout">
+          <div className="check-in">
+            <div className="switch">
+              <label for="toggle" class="toggle-label">
+                <input
+                  class="circle"
+                  id="toggle"
+                  name="toggle"
+                  type="checkbox"
+                  onChange={handleToggle}
+                />
+
+                <span class="slider">
+                  <div class="star_button star_1"></div>
+                  <div class="star_button star_2"></div>
+                  <div class="star_button star_3"></div>
+                  <svg viewBox="0 0 16 16" class="cloud_1 cloud">
+                    <path
+                      transform="matrix(.77976 0 0 .78395-299.99-418.63)"
+                      fill="#fff"
+                      d="m391.84 540.91c-.421-.329-.949-.524-1.523-.524-1.351 0-2.451 1.084-2.485 2.435-1.395.526-2.388 1.88-2.388 3.466 0 1.874 1.385 3.423 3.182 3.667v.034h12.73v-.006c1.775-.104 3.182-1.584 3.182-3.395 0-1.747-1.309-3.186-2.994-3.379.007-.106.011-.214.011-.322 0-2.707-2.271-4.901-5.072-4.901-2.073 0-3.856 1.202-4.643 2.925"
+                    ></path>
+                  </svg>
+                </span>
+              </label>
+            </div>
+          </div>
+
+          <div className="time-spent">
+            <div className={`time-spent-title ${isChecked ? "active" : ""}`}>
+              <h1>
+                {" "}
+                Time <span>{isChecked ? "Start" : "Stop"}</span>
+              </h1>
+            </div>
+            <div className="time-spent-number">
+              <span>{timeSpent}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
